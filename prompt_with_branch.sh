11228f3d4bf10995e33d1cbfb69c5ce8f003ac02
ruiholdorf <rui.holdorf@sage.com>
2019-05-01 17:45:48 -0300

    versao inicial

--- prompt_with_branch.sh
+++ prompt_with_branch.sh
@@ -9,9 +9,7 @@ COLOR_RESET="\033[0m"
 
 function git_color {
   local git_status="$(git status 2> /dev/null)"
-  if [[ ! $git_status =~ "working directory clean" ]]; then
-    echo -e $COLOR_RED
-  elif [[ $git_status =~ "Your branch is ahead of" ]]; then
+  if [[ $git_status =~ "Your branch is ahead of" ]]; then
     echo -e $COLOR_YELLOW
   elif [[ $git_status =~ "nothing to commit" ]]; then
     echo -e $COLOR_GREEN
@@ -31,11 +29,6 @@ function git_branch {
     echo " ($commit)"
   fi
 }
-
-#parse_git_branch() {
-#     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
-#}
-# export PS1="\[\033[32m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\] $ "
 
 PS1="\[$COLOR_WHITE\]\n\w"    # basename of pwd
 PS1+="\[\$(git_color)\]"        # colors git status
