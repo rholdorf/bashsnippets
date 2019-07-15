a9350c313c7c8b9ab7961ec5b25bff3925a3c23a
Rui Holdorf <rui.holdorf@sage.com>
2019-07-15 13:23:10 +0000

    simplificação, sem cores

--- prompt_with_branch.sh
+++ prompt_with_branch.sh
@@ -1,40 +1,20 @@
 #!/bin/bash
-COLOR_RED="\033[0;31m"
-COLOR_YELLOW="\033[0;33m"
-COLOR_GREEN="\033[0;32m"
-COLOR_OCHRE="\033[38;5;95m"
-COLOR_BLUE="\033[0;34m"
-COLOR_WHITE="\033[0;37m"
-COLOR_RESET="\033[0m"
-
-function git_color {
-  local git_status="$(git status 2> /dev/null)"
-  if [[ ! $gitstatus =~ "working directory clean" ]]; then
-    echo -e $COLOR_RED
-  elif [[ $git_status =~ "Your branch is ahead of" ]]; then
-    echo -e $COLOR_YELLOW
-  elif [[ $git_status =~ "nothing to commit" ]]; then
-    echo -e $COLOR_GREEN
-  else
-    echo -e $COLOR_OCHRE
-  fi
-}
-function git_branch {
-  local git_status="$(git status 2> /dev/null)"
-  local on_branch="On branch ([^${IFS}]*)"
-  local on_commit="HEAD detached at ([^${IFS}]*)"
-  if [[ $git_status =~ $on_branch ]]; then
-    local branch=${BASH_REMATCH[1]}
-    echo " $branch"
-  elif [[ $git_status =~ $on_commit ]]; then
-    local commit=${BASH_REMATCH[1]}
-    echo " $commit"
-  fi
+__git_status() {
+    STATUS=$(git status 2>/dev/null |
+    awk '
+    /^On branch / {printf($3)}
+    /^You are currently rebasing/ {printf("rebasing %s", $6)}
+    /^Initial commit/ {printf(" (init)")}
+    /^Untracked files/ {printf(" +")}
+    /^Changes not staged / {printf(" ?")}
+    /^Changes to be committed/ {printf(" *")}
+    /^Your branch is ahead of/ {printf(" ^")}
+    ')
+    if [ -n "$STATUS" ]; then
+        echo -ne " $STATUS"
+    fi
 }
 
-PS1="\[$COLOR_WHITE\]\n\w"    # basename of pwd
-#PS1+="\[\$(git_color)\]"        # colors git status
-PS1+="\$(git_branch)"           # prints current branch
-PS1+="\[$COLOR_BLUE\] $\[$COLOR_RESET\] "   # '#' for root, else '$'
+PS1='\[\033[01;34m\]\w\[\033[00m\]$(__git_status) $ '
 export PS1
 
